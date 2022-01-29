37e36f4fb98eb38e74da89b755fbca4768b22c51
Zuke27 <wstaylor27@gmail.com>
2022-01-29 16:18:29 -0500

    Added helper.js

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { checkPassword, validateEmail } from "../../utils/helpers";
+
+export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email) || !name) {
+      setErrorMessage("Email or name is invalid");
+      return;
+    }
+    if (!message) {
+      setErrorMessage(`Please enter a message`);
+      return;
+    }
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <aside>
+        <h1>Contact</h1>
+      </aside>
+      <form className="form">
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="name"
+          placeholder="name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
