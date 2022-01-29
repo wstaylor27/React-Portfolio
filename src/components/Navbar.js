bfbe95622476cd96e30f7547787b857c95726cea
Zuke27 <wstaylor27@gmail.com>
2022-01-29 11:23:22 -0500

    Updated Navbar tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,49 @@ import React from "react";
 import "../styles/style.css";
 import "../styles/reset.css";
 
-export default function Navbar() {
+export default function Navbar({ currentPage, handlePageChange }) {
   return (
     <nav>
-      <ul>
-        <li>
-          <a href="#about-me">About Me</a>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#home"
+            onClick={() => handlePageChange("Home")}
+            className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+          >
+            Home
+          </a>
         </li>
-        <li>
-          <a href="#projects">Projects</a>
+        <li className="nav-item">
+          <a
+            href="#about-me"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About Me
+          </a>
         </li>
-        <li>
-          <a href="#contact">Contact</a>
+        <li className="nav-item">
+          <a
+            href="#projects"
+            onClick={() => handlePageChange("Projects")}
+            className={
+              currentPage === "Projects" ? "nav-link active" : "nav-link"
+            }
+          >
+            Projects
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          >
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
