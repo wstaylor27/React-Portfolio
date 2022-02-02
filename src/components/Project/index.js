f54122ef2121d85e4579a75b4e5bf02393934282
Zuke27 <wstaylor27@gmail.com>
2022-02-01 22:33:48 -0500

    Updated card hover attributes

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,16 +7,20 @@ function Project({ project }) {
   //   src={require(`../../assets/projects/${name}.jpg`)}
 
   return (
-    <div className="cards card" key={name}>
+    <div className="card" key={name}>
       <img
         src={require(`../../images/${name}.jpg`)}
         alt={removeHyphensAndCapitalize(name)}
-        className="card__img"
+        className="card__img "
       />
+      <a href={link}>
+        <div className="card__img--hover"></div>
+      </a>
+
       <div className="card__info">
-        <span class="card__category">Project</span>
+        <span className="card__category">Project</span>
         <h3 className="card__title">
-          <span className="card__by">
+          <span className="card__by ">
             <a href={link}>{removeHyphensAndCapitalize(name)}</a>{" "}
             <a href={repo}>
               <i className="fab fa-github"></i>
