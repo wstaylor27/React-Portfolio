d1c18b38d904ab5418b429815917a7596f9fe9b3
Zuke27 <wstaylor27@gmail.com>
2022-02-13 22:40:28 -0500

    Updated About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,6 +10,16 @@ export default function About() {
         <h4>About Me</h4>
       </aside>
       <p>
+        Greetings world! My name is Wesley Taylor, but most call me "Wes". I was
+        born and raised in the "Queen City", also known as, Charlotte, North
+        Carolina. As a youth, I was extremely inquisitive of the world around
+        me. Everyday was an adventure to explore new experiences. I was also an
+        amateur artist. I had a passion to create visual and audio art.
+        Fast-forward to present day, I've traveled to over 20 countries! I'm
+        also an inventor, entrepreneur, commercial drone pilot, and a music
+        producer.
+      </p>
+      <p>
         So, how did I become a software developer? Better question -- why? Two
         years ago, I realized my full-time job at the time needed a change. The
         hours were good, however, the pay was insufficient. My day-to-day
